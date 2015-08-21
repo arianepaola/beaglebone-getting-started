5c1d580df86998283e591f86e51eb61b2f8563c4
Ariane Paola Gomes <arianepaola@users.noreply.github.com>
2015-08-21 18:41:14 +0200

    Added eMMC flashing

--- App/js/beagle_flash_emmc.js
+++ App/js/beagle_flash_emmc.js
@@ -1,10 +1,25 @@
-var spawn = require('child_process').spawnSync;
-var os = require('os');
-var tempWrite = require('tempwrite');
+var sshClient = require('ssh2').Client;
+var conn = new sshClient();
 
+var flasheMMC = function(host, port, username, password) {
+    conn.on('ready', function () {
+        conn.exec("sed -i 's|#cmdline=init=/opt/scripts/tools/eMMC/init-eMMC-flasher-v3.sh|cmdline=init=/opt/scripts/tools/eMMC/init-eMMC-flasher-v3.sh|g' \/boot\/uEnv.txt && reboot", function (err, stream) {
+            if (err) {
+                console.log("error");
+            }
 
-var writeFlashScript = function(origFile, dest) {
-    var filepath = tempWrite.sync('boot');
-
-    tempWrite.sync('cmdline=init=/opt/scripts/tools/eMMC/init-eMMC-flasher-v3.sh');
+            stream.on('close', function (code, signal) {
+                conn.end();
+            }).on('data', function (data) {
+                console.log('output: ' + data);
+            }).stderr.on('data', function (data) {
+                    console.log('error: ' + data);
+                });
+        }).connect({
+            host: host,
+            port: port,
+            username: username,
+            password: password
+        });
+    });
 };

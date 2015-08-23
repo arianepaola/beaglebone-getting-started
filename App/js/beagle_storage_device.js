981844c1b6d640828a58af38ff19229b8d9b402e
Ariane Paola Gomes <arianepaola@users.noreply.github.com>
2015-08-24 00:22:47 +0200

    Added module export functionality

--- App/js/beagle_storage_device.js
+++ App/js/beagle_storage_device.js
@@ -1,27 +1,38 @@
+#!/usr/bin/env node
+/*
+ * BeagleBone Getting Started Application
+ * https://github.com/arianepaola/beaglebone-getting-started/tree/nwjs
+ *
+ * Copyright (c) 2015 Ariane Paola Gomes
+ * Licensed under the MIT license.
+ */
+
 var spawn = require('child_process').spawnSync;
 var os = require('os');
 var sudo = require('sudo');
 var windosu = require('windosu');
-var tempWrite = require('tempwrite');
+var tempWrite = require('temp-write');
 
-var command_line = function(command, arguments) {
-    var command_output = spawn(command, arguments);
+// runs a command and returns its output as string
+var commandLine = function(command, arguments) {
+    var commandOutput = spawn(command, arguments);
 
-    return command_output.stdout.toString();
+    return commandOutput.stdout.toString();
 };
 
-
+// detects BeagleBone Linux device entry
 var detectDeviceLinux = function() {
-    var dmesg_output = command_line('dmesg', ['-k']);
-    var mount_output = command_line('mount', ['-l']);
+    var dmesgOutput = commandLine('dmesg', ['-k']);
+    var mountOutput = commandLine('mount', ['-l']);
 
+    // regular expressions for dmesg and mount outputs
     var reDmesg = /[\s\S]*?(.*idVendor=1d6b, idProduct=0104\n.*\n.*Product: BeagleBoneBlack\n.*Manufacturer: Circuitco\n.*SerialNumber:*[\s\S]*?Direct-Access.*Linux.*File-CD Gadget[\s\S]*?\[(sd[a-z]{1})\] 196608 512-byte logical blocks:[\s\S]*?\[(sd[a-z]{1})\] Attached SCSI removable disk)/gm;
     var reMount = /[\s\S]*?(.*\/dev\/(sd[a-z]{1}) on \/media\/.*\/BEAGLEBONE type vfat \(.*\) \[BEAGLEBONE\])/gm;
 
-    var resultDmesg = reDmesg.exec(dmesg_output);
-    var resultMount = reMount.exec(mount_output);
+    var resultDmesg = reDmesg.exec(dmesgOutput);
+    var resultMount = reMount.exec(mountOutput);
 
-
+    // check if dmesg and mount output match
     if((resultDmesg[2] == resultDmesg[3]) && (resultDmesg[2] == resultMount[2]) && (resultDmesg[2] != null)) {
         return '/dev/' + resultDmesg[2];
     }
@@ -29,23 +40,28 @@ var detectDeviceLinux = function() {
     return null;
 };
 
+// detects BeagleBone MacOS device entry
 var detectDeviceMacOS = function() {
-    var system_profiler_output = command_line('system_profiler', ['SPUSBDataType']);
-    var diskutil_list_output = command_line('diskutil', ['list']);
+    var systemProfilerOutput = commandLine('system_profiler', ['SPUSBDataType']);
+    var diskutilListOutput = commandLine('diskutil', ['list']);
 
+    // regular expressions for system_profiler and diskutil outputs
     var reSystemProfiler = /[\s\S]*?BeagleBoneBlack:\s+Product ID: (0[xX][0-9a-fA-F]{4})\s+Vendor ID: (0[xX][0-9a-fA-F]{4})/gm;
     var reDiskutilList = /[\s\S]*?\/dev\/(disk[0-9]{1})\s+.*TYPE NAME\s+SIZE\s+IDENTIFIER\s+0:\s+BEAGLEBONE\s+\*[0-9]+\.[0-9]+\s+MB\s+(disk[0-9]{1})/gm;
     var reDiskutilInfo = /[\s\S]*?.*Device Identifier:\s+(disk[0-9]{1})\s+Device Node:\s+(\/dev\/disk[0-9]{1})[\s\S]*?Linux File-CD Gadget Media\s+Volume Name:\s+BEAGLEBONE\s+Mounted:\s+(Yes|No)\s+Mount Point:\s+(\/Volumes\/.*)/gm;
 
-    var resultSystemProfiler = reSystemProfiler.exec(system_profiler_output);
-    var resultDiskutilList = reDiskutilList.exec(diskutil_list_output);
+    var resultSystemProfiler = reSystemProfiler.exec(systemProfilerOutput);
+    var resultDiskutilList = reDiskutilList.exec(diskutilListOutput);
 
+    // check if the USB device and vendor ids match, and compare with diskutil output
     if((resultSystemProfiler[1] == '0x0104' && resultSystemProfiler[2] == '0x1d6b') &&
         ((reDiskutilList[1] == reDiskutilList[2]) && (reDiskutilList[1] != null))) {
 
-        var diskutil_list_output = command_line('diskutil', ['info', resultSystemProfiler[1]]);
-        var resultDiskutilInfo = reDiskutilInfo.exec(diskutil_list_output);
+        // verify additional diskutil info for the device match
+        var diskutilInfoOutput = commandLine('diskutil', ['info', resultSystemProfiler[1]]);
+        var resultDiskutilInfo = reDiskutilInfo.exec(diskutilInfoOutput);
 
+        // check for a matching diskutil info and diskutil list device
         if(resultDiskutilInfo[1] == resultDiskutilList[1]) {
             // return device node
             return resultDiskutilInfo[2];
@@ -55,16 +71,17 @@ var detectDeviceMacOS = function() {
     return null;
 };
 
+// detects BeagleBone Windows device entry
 var detectDeviceWindows = function() {
+    var wmicOutput = commandLine('wmic', ['diskdrive', 'list']);
+    var ddOutput = commandLine('dd.exe', ['--list']);
 
-    var wmic_output = command_line('wmic', ['diskdrive', 'list']);
-    var dd_output = command_line('dd.exe', ['--list']);
-
+    // regular expressions for wmic and dd for windows
     var reWmic = /[\s\S]*?Linux File-CD Gadget USB Device\s+(.*)\s+USBSTOR/gm;
     var reDd = /[\s\S]*?Volume\{(.*)\}\\\s+.*\s+removeable media\s+Mounted on\s+(\\.*)\s+/gm;
 
-    var resultWmic = reWmic.exec(wmic_output);
-    var resultDd = reDd.exec(dd_output);
+    var resultWmic = reWmic.exec(wmicOutput);
+    var resultDd = reDd.exec(ddOutput);
 
     if(resultWmic[1] != null && resultDd[1] != null) {
         return resultWmic[1];
@@ -74,6 +91,7 @@ var detectDeviceWindows = function() {
 
 };
 
+// remove / umount BeagleBone device Linux, Mac and Windows
 var umount = function(device, password) {
     // Linux
     if(os.type().substring(0,4) == 'Linux') {
@@ -94,6 +112,7 @@ var umount = function(device, password) {
     }
 };
 
+// umount a specified device on Linux
 var umountLinux = function(device, password) {
     var options = {cachePassword: false, password: password};
 
@@ -104,6 +123,7 @@ var umountLinux = function(device, password) {
     });
 };
 
+// umount a specified device on MacOS
 var umountMacOS = function(device, password) {
     var options = {cachePassword: false, password: password};
 
@@ -114,6 +134,7 @@ var umountMacOS = function(device, password) {
     });
 };
 
+// umount a specified device on Windows
 var umountWindows = function(device, password) {
     // write diskpart command script
     var filepath = tempWrite.sync('diskpart_script.txt');
@@ -122,8 +143,15 @@ var umountWindows = function(device, password) {
 
     windosu.exec('diskpart /s ' + fs.readFileSync(filepath, 'utf8'));
 
+    // write diskpart umount script
     var umountScript = tempWrite.sync('diskpart_umount.txt');
     tempWrite.sync('list volume\r\nselect volume 3\r\nremove all dismount\r\nexit');
 
     windosu.exec('diskpart /s ' + fs.readFileSync(umountScript, 'utf8'));
 };
+
+// export module functionality
+module.exports.detectDeviceLinux = detectDeviceLinux;
+module.exports.detectDeviceMacOS = detectDeviceMacOS;
+module.exports.detectDeviceWindows = detectDeviceWindows;
+module.exports.umount = umount;
